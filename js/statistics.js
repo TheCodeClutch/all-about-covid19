0a4c68068fadfea88ca502012b3c213d8660783f
Nikhil Singh <nikhilsingh498@gmail.com>
2020-04-27 22:34:45 +0530

    Add auth and proper google auth link

--- js/statistics.js
+++ js/statistics.js
@@ -22,29 +22,6 @@ function resize() {
 	}
 }
 
-function getToken(currToken) {
-	console.log(currToken)
-	var dataPromise = fetch('https://thecodeclutch.herokuapp.com/auth/getToken', {
-		method: 'GET',
-		headers: {
-			Authorization: currToken
-		}
-	});
-	dataPromise.then(data => {
-		return data.json()
-	})
-	.then( val => {
-		localStorage.setItem('TCC_SID', val.token)
-	})
-	.catch( err => {
-		// Token refresh error
-	})
-}
-
-window.setInterval(function(){
-  getToken(localStorage.getItem('ABC19_SID'))
-}, 720000);
-
 fetch('https://api.covid19india.org/data.json')
 .then(res => {
 	console.log(res)
