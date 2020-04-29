2861d37e7321cef111d4f98fd63a7bc0f76732f4
Nikhil Singh <nikhilsingh498@gmail.com>
2020-04-29 06:35:07 +0530

    Refactor

---
+++ js/help.js
@@ -0,0 +1,178 @@
+document.getElementById("preloader").style.display = "block";
+async function loginCheck() {
+	if (localStorage.getItem('ABC19_SID') !== null) {
+		let resp = await fetch("https://allaboutcovid-19.herokuapp.com/auth/isloggedin", {
+			method: 'GET',
+			headers: {
+				Authorization: localStorage.getItem('ABC19_SID')
+			}
+		})
+		let res = await resp.json()
+		if (res.status === 401) {
+			document.getElementById("login").style.display = "block";
+			localStorage.setItem('ABC19_login', false);
+		}
+		else {
+			localStorage.setItem('ABC19_login', true);
+			document.getElementById('user-name').innerHTML = res.name;
+			document.getElementById('user-pic').innerHTML = `<img src="${res.profile_pic}" style="height : 40px; width : 40px; border-radius : 50%; margin-right : 15px">`
+		}
+
+	} else {
+		document.getElementById("login").style.display = "block";
+		localStorage.setItem('ABC19_login', false);
+	}
+}
+async function updateLoginWithUrl(url) {
+	let name = await url.searchParams.get("name");
+	let email = await url.searchParams.get("email");
+	let sessionID = await url.searchParams.get("sessionID");
+	let image = await url.searchParams.get("image");
+	localStorage.setItem('ABC19_SID', sessionID);
+	localStorage.setItem('ABC19_login', true);
+	document.getElementById('user-name').innerHTML = name;
+	document.getElementById('user-pic').innerHTML = `<img src="${image}" style="height : 40px; width : 40px; border-radius : 50%; margin-right : 15px">`
+}
+
+function updateStorageVar() {
+	if (localStorage.getItem('ABC19_SID') !== null) {
+		localStorage.setItem('ABC19_login', true);
+	} else {
+		localStorage.setItem('ABC19_login', false);
+	}
+}
+
+function fieldReset(e) {
+	e.target.style.borderColor = 'lightgray'
+	document.getElementById('submit-btn').innerHTML = "Send Request";
+}
+
+function getFormattedTime() {
+	let date = new Date();
+	let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	var dateString = date.getDate() + " " + monthNames[date.getMonth()] + " " + date.getFullYear() + ", " + date.getHours() + ":" + ("00" + date.getMinutes()).slice(-2)
+	return dateString;
+}
+
+function validate() {
+	let isValid = true;
+	if (document.getElementById('stateId').value.length === 0) {
+		document.getElementById('stateId').style.borderColor = '#FF073A'
+		isValid = false;
+	}
+	if (document.getElementById('cityId').value.length === 0) {
+		document.getElementById('cityId').style.borderColor = '#FF073A'
+		isValid = false;
+	}
+	if (document.getElementById('title').value.length === 0) {
+		document.getElementById('title').style.borderColor = '#FF073A'
+		isValid = false;
+	}
+	if (document.getElementById('message').value.length <= 100) {
+		document.getElementById('message').style.borderColor = '#FF073A'
+		isValid = false;
+	}
+	if (isValid) {
+		return true;
+	}
+	return false;
+}
+
+let url_string = window.location.href;
+let url = new URL(url_string);
+if (url.searchParams.get("name") !== null) {
+	updateLoginWithUrl(url);
+} else {
+	loginCheck();
+}
+window.onload = function () {
+	document.getElementById("preloader").style.display = "none";
+	var quotes = ["#StaySafe", "#StayAtHome", "#SocialDistancing", "#IndiaFightsCorona", "#SafeHands", "#TogetherAtHome", "#QuarantineAndChill",
+		"#FlattenTheCurve", "#Lockdown", "#WorkingFromHome", "#ViewFromMyWindow", "#MyPandemicSurvivalPlan", "#IndiaFightsBack", "#WithMe",
+		"#EverydayIsASunday", "#CoronaWarriors", "#MyGovFactCheck"];
+
+	for (let i = 0; i < quotes.length; i++) {
+		setTimeout(function () {
+			document.getElementById("hashtag").innerHTML = quotes[i];
+		}, i * 4000);
+	}
+	updateStorageVar();
+	document.getElementById('submit-btn').addEventListener('click', () => {
+		if (localStorage.getItem('ABC19_login') === 'true') {
+			if (validate()) {
+				document.getElementById('submit-btn').innerHTML = "Sending...";
+				let data = {
+					time_ms: Date.now(),
+					time_formatted: getFormattedTime(),
+					state: document.getElementById('stateId').value,
+					city: document.getElementById('cityId').value,
+					title: document.getElementById('title').value,
+					description: document.getElementById('message').value,
+				}
+				fetch('https://allaboutcovid-19.herokuapp.com/posts/saveposts', {
+					method: 'POST',
+					headers: {
+						'content-type': 'application/json',
+						'Authorization': localStorage.getItem('ABC19_SID')
+					},
+					body: JSON.stringify(data)
+				})
+					.then(res => res.json())
+					.then(data => {
+						if (data.msg !== undefined) {
+							Swal.fire({
+								icon: 'success',
+								title: 'Yayayay',
+								text: data.msg
+							})
+							document.getElementById('submit-btn').innerHTML = "Send Request";
+						} else {
+							Swal.fire({
+								icon: 'error',
+								title: 'Oops...',
+								text: data.err,
+								footer: '<a href="./userrequest.html">Show my requests</a>'
+							})
+							document.getElementById('submit-btn').innerHTML = "Send Request";
+						}
+
+					})
+			}
+		}
+		else {
+			Swal.fire({
+				title: 'Login',
+				text: "You need to be logged in before posting",
+				icon: 'info',
+				showClass: {
+					popup: 'animated fadeInDown faster'
+				},
+				body: JSON.stringify(data)
+			})
+				.then(res => res.json())
+				.then(data => {
+					if (data.msg !== undefined) {
+						Swal.fire({
+							icon: 'success',
+							title: 'Yayayay',
+							text: data.msg
+						})
+						document.getElementById('submit-btn').innerHTML = "Send Request";
+					} else {
+						Swal.fire({
+							icon: 'error',
+							title: 'Oops...',
+							text: data.err
+						})
+						document.getElementById('submit-btn').innerHTML = "Send Request";
+					}
+				})
+
+			document.getElementsByClassName('swal2-confirm swal2-styled')[0].innerHTML = `<img width="20px" style="margin-bottom: 2px; margin-right: 2px;" src="./assets/google.webp"
+                            alt="Google sign-in">
+                        Login with Google`
+			document.getElementsByClassName('swal2-confirm swal2-styled')[0].style.color = '#000000'
+		}
+	})
+}
+
---
+++ js/showrequest.js
@@ -0,0 +1,247 @@
+document.getElementById("preloader").style.display = "block";
+
+async function loginCheck() {
+	if (localStorage.getItem('ABC19_SID') !== null) {
+		let resp = await fetch("https://allaboutcovid-19.herokuapp.com/auth/isloggedin", {
+			method: 'GET',
+			headers: {
+				Authorization: localStorage.getItem('ABC19_SID')
+			}
+		})
+		let res = await resp.json()
+		if (res.status === 401) {
+			document.getElementById("login").style.display = "block";
+			localStorage.setItem('ABC19_login', false);
+		}
+		else {
+			localStorage.setItem('ABC19_login', true);
+			document.getElementById('user-name').innerHTML = res.name;
+			document.getElementById('user-pic').innerHTML = `<img src="${res.profile_pic}" style="height : 40px; width : 40px; border-radius : 50%; margin-right : 15px">`
+		}
+
+	} else {
+		document.getElementById("login").style.display = "block";
+		localStorage.setItem('ABC19_login', false);
+	}
+}
+async function updateLoginWithUrl(url) {
+	let name = await url.searchParams.get("name");
+	let email = await url.searchParams.get("email");
+	let sessionID = await url.searchParams.get("sessionID");
+	let image = await url.searchParams.get("image");
+	localStorage.setItem('ABC19_SID', sessionID);
+	localStorage.setItem('ABC19_login', true);
+	document.getElementById('user-name').innerHTML = name;
+	document.getElementById('user-pic').innerHTML = `<img src="${image}" style="height : 40px; width : 40px; border-radius : 50%; margin-right : 15px">`
+}
+
+function updateStorageVar() {
+	if (localStorage.getItem('ABC19_SID') !== null) {
+		localStorage.setItem('ABC19_login', true);
+	} else {
+		localStorage.setItem('ABC19_login', false);
+	}
+}
+
+function fieldReset(e) {
+	e.target.style.borderColor = 'lightgray'
+	document.getElementById('submit-btn').innerHTML = "View all requests";
+}
+
+function getFormattedTime() {
+	let date = new Date();
+	let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	var dateString = date.getDate() + " " + monthNames[date.getMonth()] + " " + date.getFullYear() + ", " + date.getHours() + ":" + ("00" + date.getMinutes()).slice(-2)
+	return dateString;
+}
+
+function validate() {
+	let isValid = true;
+	if (document.getElementById('stateId').value.length === 0) {
+		document.getElementById('stateId').style.borderColor = '#FF073A'
+		isValid = false;
+	}
+	if (document.getElementById('cityId').value.length === 0) {
+		document.getElementById('cityId').style.borderColor = '#FF073A'
+		isValid = false;
+	}
+	if (isValid) {
+		return true;
+	}
+	return false;
+}
+
+let url_string = window.location.href;
+let url = new URL(url_string);
+if (url.searchParams.get("name") !== null) {
+	updateLoginWithUrl(url);
+} else {
+	loginCheck();
+}
+window.onload = function () {
+	document.getElementById("preloader").style.display = "none";
+	var quotes = ["#StaySafe", "#StayAtHome", "#SocialDistancing", "#IndiaFightsCorona", "#SafeHands", "#TogetherAtHome", "#QuarantineAndChill",
+		"#FlattenTheCurve", "#Lockdown", "#WorkingFromHome", "#ViewFromMyWindow", "#MyPandemicSurvivalPlan", "#IndiaFightsBack", "#WithMe",
+		"#EverydayIsASunday", "#CoronaWarriors", "#MyGovFactCheck"];
+
+	for (let i = 0; i < quotes.length; i++) {
+		setTimeout(function () {
+			document.getElementById("hashtag").innerHTML = quotes[i];
+		}, i * 4000);
+	}
+	updateStorageVar();
+	document.getElementById('submit-btn').addEventListener('click', () => {
+		if (validate()) {
+			document.getElementById('submit-btn').innerHTML = "Requesting...";
+			let data = {
+				state: document.getElementById('stateId').value,
+				city: document.getElementById('cityId').value,
+			}
+			fetch('https://allaboutcovid-19.herokuapp.com/posts/getposts', {
+				method: 'POST',
+				headers: {
+					'content-type': 'application/json',
+				},
+				body: JSON.stringify(data)
+			})
+				.then(res => res.json())
+				.then(resp => {
+					if (resp.msg !== undefined) {
+						let data = resp.msg;
+						let content = "";
+						for (let i = data.length - 1; i >= 0; i = i - 1) {
+							content = content + `<div data-aos="zoom-out"  style="margin-top : 20px; margin-bottom : 20px">
+												<div class="card" style="width: 100%">
+													<div class="card-body">
+														<div style="display: inline-block;">
+															<img src='${data[i].IMAGE_URL}' style="height: 60px; width : 60px; border-radius: 50%; margin-right : 20px;">
+														</div>
+														<div style="display: inline-block;transform: translateY(25%);">
+															<h5 class="card-title">${data[i].TITLE}</h5>
+															<h6 class="card-subtitle mb-2 text-muted">by ${data[i].NAME}</h6>
+														</div>
+														<div class="time" style="float: right; transform: translateY(50%);">
+												<span>${data[i].TIME_FORMATTED}</span>
+														</div>       
+														<div style="margin-top : 20px">
+														<p class="card-text" style="text-align: justify">${data[i].DESCRIPTION}</p>
+													</div>
+													<div class="col-12" style="margin-top : 20px">
+														<input id='comm-attr-${data[i].POST_ID}' type="text" style="width: 100%; margin-right : 20px; padding : 3px; margin-top : 10px" >
+													</div>
+													<div class="col-12" >
+														<div style="float : right">
+														<button class="btn btn-primary" data-abc19-post-id='${data[i].POST_ID}' onclick="postComment(event)" data-comment-attr='comm-attr-${data[i].POST_ID}'  style="padding : 3px 20px 3px 20px; min-width : 40%; transform : translateY(-5%); margin-top : 10px">Post</button>
+															</div>
+															<div style="float: left; padding-top : 13px">
+																<a class="card-link" onclick="changeText(event)" data-toggle="collapse" href='#${data[i].POST_ID}' role="button" aria-expanded="false" aria-controls="collapseExample">Show comments</a>
+															</div>
+														</div>
+													</div>
+												</div>
+												<div class="collapse" id='${data[i].POST_ID}'>`
+
+							for (let j = 0; j < data[i].COMMENTS.length; j = j + 1) {
+
+								content = content + `														
+												<div class="card" style="padding-left : 40px">
+														<div class="card-metadata card-body">
+															<div class="name-email" style="float: left;">
+																<img src='${data[i].COMMENTS[j].PIC_URL}' style="height: 40px; width : 40px; border-radius: 50%; margin-right : 20px"><span class="name">${data[i].COMMENTS[j].NAME}</span>
+															</div>
+															<div class="date time" style="float: right; transform: translateY(25%);">
+																${data[i].COMMENTS[j].TIME_FORMATTED}
+															</div>
+														</div>
+														<div class="card-body" style="padding-top : 0; text-align: justify;">
+															${data[i].COMMENTS[j].COMMENT_DESC}
+														</div>
+													</div>`
+							}
+							content = content + `</div></div>`
+
+						}
+						document.getElementById('submit-btn').innerHTML = "View all requests"
+						document.getElementById('post-heading').innerHTML = "Posts"
+						document.getElementById('posts').style.display = 'block';
+						document.getElementById('posts-container').innerHTML = content;
+					}
+				})
+
+		}
+	})
+}
+
+function changeText(event) {
+	if (event.target.innerHTML === "Show comments") {
+		event.target.innerHTML = "Hide comments"
+	} else if (event.target.innerHTML === "Hide comments") {
+		event.target.innerHTML = "Show comments"
+	}
+}
+
+function postComment(event) {
+	let idVal = event.target.getAttribute('data-comment-attr');
+	if (document.getElementById(String(idVal)).value.length < 1) {
+		document.getElementById(String(idVal)).style.borderColor = '#FF073A'
+		return;
+	}
+	if (localStorage.getItem('ABC19_login') === 'true') {
+		event.target.innerHTML = "Posting..."
+		let data = {
+			time_ms: Date.now(),
+			time_formatted: getFormattedTime(),
+			description: document.getElementById(String(idVal)).value,
+			post_id: event.target.getAttribute('data-abc19-post-id')
+		}
+		fetch('https://allaboutcovid-19.herokuapp.com/posts/savecomment', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+				'Authorization': localStorage.getItem('ABC19_SID')
+			},
+			body: JSON.stringify(data)
+		})
+			.then(res => { return res.json() })
+			.then(message => {
+				event.target.innerHTML = "Post"
+				if (message.msg !== undefined) {
+					Swal.fire({
+						icon: 'success',
+						title: 'Yayayay',
+						text: message.msg
+					})
+				} else {
+					Swal.fire({
+						icon: 'error',
+						title: 'Oops...',
+						text: message.err
+					})
+				}
+			})
+	} else {
+		Swal.fire({
+			title: 'Login',
+			text: "You need to be logged in before posting",
+			icon: 'info',
+			showClass: {
+				popup: 'animated fadeInDown faster'
+			},
+			hideClass: {
+				popup: 'animated fadeOutUp faster'
+			},
+			showCancelButton: false,
+			confirmButtonColor: '#FFFFFF',
+			cancelButtonColor: '#FF073A',
+			confirmButtonText: 'Login with Google'
+		}).then((result) => {
+			if (result.value) {
+				window.location.href = 'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&client_id=494631050345-uov5hrao1lv23e78bpmbn6t5argm5qug.apps.googleusercontent.com&prompt=consent&redirect_uri=https%3A%2F%2Fallaboutcovid-19.herokuapp.com%2Fauth%2Fgoogle&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile';
+			}
+		})
+		document.getElementsByClassName('swal2-confirm swal2-styled')[0].innerHTML = `<img width="20px" style="margin-bottom: 2px; margin-right: 2px;" src="./assets/google.webp"
+													alt="Google sign-in">
+											Login with Google`
+		document.getElementsByClassName('swal2-confirm swal2-styled')[0].style.color = '#000000'
+	}
+}
